c20ff1ee85f277b453e958baa97093a68ec30274
yoidea <yoidea.cc@gmail.com>
2018-05-29 12:57:52 +0900

    Test

--- ghostwriter/util.py
+++ ghostwriter/util.py
@@ -3,6 +3,37 @@ import random
 import re
 
 
+class Chain:
+    chain = {}
+    def load(self, fname='text.txt'):
+        with open(fname, 'r') as f:
+            text = f.read()
+        mecab = MeCab.Tagger('-Owakati')
+        words = mecab.parse(text)
+        words = re.findall('\w+', words)
+        # return words
+
+        self.chain = {}
+        for i in range(len(words)-1):
+            if words[i] in self.chain:
+                self.chain[words[i]].append(words[i+1])
+            else :
+                self.chain[words[i]] = []
+                self.chain[words[i]].append(words[i+1])
+        # return self.chain
+
+    def make(self, wc=20):
+        selected, _ = random.choice(list(self.chain.items()))
+        sentence = selected
+        for i in range(wc):
+            if selected in self.chain:
+                selected = random.choice(self.chain[selected])
+                sentence += selected
+            else :
+                selected, _ = random.choice(list(self.chain.items()))
+        return sentence
+
+
 def make_sentence(chain, wc=20):
     selected, _ = random.choice(list(chain.items()))
     sentence = selected
@@ -36,11 +67,9 @@ def get_words(fname='text.txt'):
 
 
 def main():
-    words = get_words()
-    print(words)
-    chain = build_chain(words)
-    print(chain)
-    sentence = make_sentence(chain)
+    chain = Chain()
+    chain.load('../text.txt')
+    sentence = chain.make(wc=20)
     print(sentence)
 
 
